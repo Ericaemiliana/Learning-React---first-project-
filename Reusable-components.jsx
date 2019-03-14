59ba8930c6afd56fc1031471cf1bb39c2dafc81b
Erica Pantoja <emilianapantoja90@gmail.com>
2019-03-14 16:14:38 -0500

    Update Reusable-components.jsx

--- Reusable-components.jsx
+++ Reusable-components.jsx
@@ -1,48 +1,90 @@
-// Code goes here
+
 var Product = React.createClass({
-  getInitialState: function(){
+
+  getInitialState: function() {
+
     return {qty: 0};
+
   },
-  
-  buy: function(){
-    this.setState({qty: this.state.qty + 1});
+
+
+
+  buy: function() {
+
+    this.setState({qty: this.state.qty +1});
+
   },
-  
-  render: function(){
-    return(
-    <div>
-      <p> android --- $199</p>
-      <button onClick= {this.buy}>Buy</button>
-      <h3>Qty: {this.state.qty} item(s)</h3>
-      </hr>
-    </div>
-  );
+
+
+
+  render: function() {
+
+    return (
+
+      <div>
+
+        <h3>Android - $199</h3>
+
+        <button onClick={this.buy}>Buy</button>
+
+        <h4>Qty: {this.state. qty} item(s)</h4>
+
+        <hr/>
+
+      </div>
+
+    );
+
   }
+
 });
+
+
 
 var Total = React.createClass({
-  render: function(){
+
+  render: function() {
+
     return (
+
       <div>
+
         <h3>Total Cash: </h3>
+
       </div>
-      );
+
+    );
+
   }
-  
+
 });
+
+
 
 var ProductList = React.createClass({
-  render: function(){
+
+  render: function() {
+
     return (
+
       <div>
+
         <Product/>
+
         <Product/>
+
         <Product/>
+
         <Total/>
+
       </div>
-      );
+
+    );
+
   }
-  
+
 });
 
-React.render(<Productlist/>, document.getElementById('root'));
+
+
+React.render(<ProductList/>, document.getElementById("root"));
